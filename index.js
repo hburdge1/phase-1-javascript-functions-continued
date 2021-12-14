60e0f8d7e49db21b47ec0d31743b2851c8d2e346
Hannah Burdge <hburdge1@gmail.com>
2021-12-14 09:17:39 -0800

    commit tree 6a271b8fa37a8fe0c52ca68d4b971e3740b68ef2

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// code your solution here
+function saturdayFun(bits = 'roller-skate'){
+    return `This Saturday, I want to ${bits}!` ;
+};
+function mondayWork(work = 'go to the office'){
+    return `This Monday, I will ${work}.` ;
+}
+
+function wrapAdjective(flair = '*') {
+    return function(result = 'special') {
+       return `You are ${flair}${result}${flair}!`;
+    }
+}
